f0fb30de85550401072d9e5e662d0a7bdc987ac3
reschandreas <resch.andreas@hotmail.it>
2016-02-03 11:09:06 +0100

    added getter- and settermethods at class Adresse

--- src/Adressbuch.java
+++ src/Adressbuch.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Andreas on 03.02.16.
  */
 public class Adressbuch {
+
+    List<Adresse> adresses = new ArrayList<>();
+    int current;
+    int amount = adresses.size();
+    String path;
 }
---
+++ src/Adresse.java
@@ -0,0 +1,96 @@
+/**
+ * Created by Andreas on 03.02.16.
+ */
+public class Adresse {
+
+    private String name;
+    private String surname;
+    private String location;
+    private String zipcode;
+    private String country;
+    private String street;
+    private int housenr;
+    private String mobilenr;
+    private String telnr;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHousenr() {
+        return housenr;
+    }
+
+    public void setHousenr(int housenr) {
+        this.housenr = housenr;
+    }
+
+    public String getMobilenr() {
+        return mobilenr;
+    }
+
+    public void setMobilenr(String mobilenr) {
+        this.mobilenr = mobilenr;
+    }
+
+    public String getTelnr() {
+        return telnr;
+    }
+
+    public void setTelnr(String telnr) {
+        this.telnr = telnr;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
